7fe4f0d85cf2fa775dbafb535cc1245878d86dbb
Landry Irakoze <irakozel@gmail.com>
2019-08-16 11:33:40 -0400

    imported dependencies

--- smurfs/src/index.js
+++ smurfs/src/index.js
@@ -1,6 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import "./index.css";
 import App from "./components/App";
+
+
 
 ReactDOM.render(<App />, document.getElementById("root"));
