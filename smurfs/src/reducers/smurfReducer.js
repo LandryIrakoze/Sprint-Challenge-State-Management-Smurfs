1b844723a62abef6e03005e3feefdf6324b4ad5e
Landry Irakoze <irakozel@gmail.com>
2019-08-16 11:58:38 -0400

    finished reducer, and initial state

--- smurfs/src/reducers/smurfReducer.js
+++ smurfs/src/reducers/smurfReducer.js
@@ -1,8 +1,37 @@
-const initialState = [
-]
+import {
+    FETCH_SMURF_DATA_START,
+    FETCH_SMURF_DATA_SUCCESS,
+    FETCH_SMURF_DATA_FAILURE
+} from '../actions/smurfActions';
+
+const initialState = {
+    smurfs: [],
+    isLoading: false,
+    error: ''
+}
 
 export const smurfReducer = (state = initialState, action) => {
     switch(action.type) {
-        
+        case FETCH_SMURF_DATA_START:
+            return {
+                ...state,
+                isLoading: true,
+                error: ''
+            }
+        case FETCH_SMURF_DATA_SUCCESS:
+            return {
+                ...state,
+                isLoading: false,
+                error: '',
+                smurfs: [...state, action.payload]
+            }
+        case FETCH_SMURF_DATA_FAILURE:
+            return {
+                ...state,
+                isLoading: false,
+                error: action.payload
+            }
+        default:
+            return state;
     }
 }
